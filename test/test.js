adfd31bea25e41575bab6f65f66011247cefe593
Liu <lc-soft@live.cn>
2019-09-29 21:09:05 +0800

    test: update error handling

--- test/test.js
+++ test/test.js
@@ -8,16 +8,16 @@ describe('i18n', function () {
   describe('compiler', function () {
     const sourceFile = path.join(__dirname, 'locales.c')
     const headerFile = path.join(__dirname, 'locales.h')
-  
+
     if (fs.existsSync(sourceFile)) {
       fs.unlinkSync(sourceFile)
     }
     if (fs.existsSync(headerFile)) {
       fs.unlinkSync(headerFile)
     }
-  
+
     compile(path.join(__dirname, 'locales.js'))
-  
+
     it(`should output ${sourceFile}`, () => {
       assert.equal(fs.existsSync(sourceFile), true)
     })
@@ -25,46 +25,40 @@ describe('i18n', function () {
       assert.equal(fs.existsSync(headerFile), true)
     })
   })
-  
+
   describe('app', function () {
     it('should be compiled successfully', () => {
       execSync('gcc -Werror -o test test.c `pkg-config --libs lcui`', { cwd: __dirname })
       assert.ok(true, 'compiled')
     })
     it('should check arguments', () => {
-      let err = false
-  
       try {
         execSync('./test', { cwd: __dirname })
-      } catch {
-        err = true
+        assert.fail('no error output')
+      } catch (err) {
+        assert.ok(true, err.message)
       }
-      assert.equal(err, true)
     })
     it('should check locale exists', () => {
-      let err = false
-  
       try {
         execSync('./test test hello', { cwd: __dirname })
-      } catch {
-        err = true
+        assert.fail('no error output')
+      } catch (err) {
+        assert.ok(true, err.message)
       }
-      assert.equal(err, true)
     })
     it('should check text exists', () => {
-      let err = false
-  
       try {
         execSync('./test en hello', { cwd: __dirname })
-      } catch {
-        err = true
+        assert.fail('no error output')
+      } catch (err) {
+        assert.ok(true, err.message)
       }
-      assert.equal(err, true)
     })
     it('should print translation text', () => {
       let enText = ''
       let cnText = ''
-  
+
       try {
         enText = execSync('./test en message.hello', { cwd: __dirname }).toString()
         cnText = execSync('./test cn message.hello', { cwd: __dirname }).toString()
@@ -74,5 +68,5 @@ describe('i18n', function () {
       assert.equal(enText, 'hello world\n')
       assert.equal(cnText, '你好，世界\n')
     })
-  })  
+  })
 })
