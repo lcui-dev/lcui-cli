d63c43c5f85195d6d753d58e5f9ba9cb111568e1
Liu <lc-soft@live.cn>
2021-05-09 13:56:47 +0800

    style: format code and fix require() warning

--- compilers/i18n/index.js
+++ compilers/i18n/index.js
@@ -1,87 +1,87 @@
-const fs = require('fs')
-const path = require('path')
-const { flat, generateFile, FileOperateLogger } = require('../../lib/utils')
+const fs = require('fs');
+const path = require('path');
+const {
+  flat, generateFile, FileOperateLogger, loadConfigFile
+} = require('../../lib/utils');
 
-const TEMPLATE_DIR = path.resolve(__dirname, 'templates')
-const TEMPLATE_FILE_C = path.join(TEMPLATE_DIR, 'i18n.c')
-const TEMPLATE_FILE_H = path.join(TEMPLATE_DIR, 'i18n.h')
+const TEMPLATE_DIR = path.resolve(__dirname, 'templates');
+const TEMPLATE_FILE_C = path.join(TEMPLATE_DIR, 'i18n.c');
+const TEMPLATE_FILE_H = path.join(TEMPLATE_DIR, 'i18n.h');
 
 function convertToC(arr, indent = 0) {
-  const body = []
+  const body = [];
 
   arr.forEach((item, i) => {
     if (item instanceof Array) {
-      body.push(...convertToC(item, indent + 1))
+      body.push(...convertToC(item, indent + 1));
     } else {
-      item = item === null ? 'NULL' : `L${JSON.stringify(item)}`
-      body.push(`${'\t'.repeat(indent + 1)}${item}`)
-      if (i < arr.length - 1) {
-      }
+      const value = item === null ? 'NULL' : `L${JSON.stringify(item)}`;
+      body.push(`${'\t'.repeat(indent + 1)}${value}`);
     }
     if (i < arr.length - 1) {
-      body.push(`${body.pop()},`)
+      body.push(`${body.pop()},`);
     }
-  })
-  return [`${'\t'.repeat(indent)}{`, ...body, `${'\t'.repeat(indent)}}`]
+  });
+  return [`${'\t'.repeat(indent)}{`, ...body, `${'\t'.repeat(indent)}}`];
 }
 
 class Compiler {
   constructor({ cwd, sourceDir } = {}) {
-    this.cwd = cwd
-    this.name = 'i18n'
-    this.logger = new FileOperateLogger(cwd)
-    this.sourceDir = sourceDir ? sourceDir : path.join(this.cwd, 'src', 'lib')
+    this.cwd = cwd;
+    this.name = 'i18n';
+    this.logger = new FileOperateLogger(cwd);
+    this.sourceDir = sourceDir || path.join(this.cwd, 'src', 'lib');
   }
 
   compile(input) {
-    const locales = require(input)
-    const localeKeys = Object.keys(locales)
-    const output = path.join(this.sourceDir, this.name)
+    const locales = loadConfigFile(input);
+    const localeKeys = Object.keys(locales);
+    const output = path.join(this.sourceDir, this.name);
 
-    let maxItems = 1
-    let content = convertToC(
+    let maxItems = 1;
+    const content = convertToC(
       localeKeys.map((key) => {
-        const result = [key]
-        const props = flat(locales[key])
+        const result = [key];
+        const props = flat(locales[key]);
 
         Object.keys(props).forEach((p) => {
-          result.push(p, props[p])
-        })
-        result.push(null)
+          result.push(p, props[p]);
+        });
+        result.push(null);
         if (result.length > 0) {
-          maxItems = result.length
+          maxItems = result.length;
         }
-        return result
+        return result;
       })
     )
-    .map((line) => {
-      let i = line.length - 1
-      if (line[i] === '[') {
-        return line.substr(0, line.length - 1) + '{'
-      }
-      if (line[i] === ']') {
-        return line.substr(0, line.length - 1) + '}'
-      }
-      if (line[i] === ',' && line[i - 1] === ']') {
-        return line.substr(0, line.length - 2) + '},'
-      }
-      return line
-    })
-    .join('\n')
+      .map((line) => {
+        const i = line.length - 1;
+        if (line[i] === '[') {
+          return `${line.substr(0, line.length - 1)}{`;
+        }
+        if (line[i] === ']') {
+          return `${line.substr(0, line.length - 1)}}`;
+        }
+        if (line[i] === ',' && line[i - 1] === ']') {
+          return `${line.substr(0, line.length - 2)}},`;
+        }
+        return line;
+      })
+      .join('\n');
 
     if (!fs.existsSync(this.sourceDir)) {
-      throw new Error(`${this.sourceDir} is not exists!`)
+      throw new Error(`${this.sourceDir} is not exists!`);
     }
-    this.logger.log('output', `${output}.c`)
+    this.logger.log('output', `${output}.c`);
     generateFile(TEMPLATE_FILE_C, `${output}.c`, {
       len: localeKeys.length,
-      content: content,
-      maxItems: maxItems,
+      content,
+      maxItems,
       fileName: this.name
-    })
-    this.logger.log('output', `${output}.h`)
-    fs.copyFileSync(TEMPLATE_FILE_H, `${output}.h`)
+    });
+    this.logger.log('output', `${output}.h`);
+    fs.copyFileSync(TEMPLATE_FILE_H, `${output}.h`);
   }
 }
 
-module.exports = Compiler
+module.exports = Compiler;
