cda954ec145485627f1a8429070ca67a4bb04adc
Liu <lc-soft@live.cn>
2023-06-22 17:12:01 +0800

    feat: delete i18n compiler and generator
    BREAKING CHANGE: The 3. x version of LCUI has an i18n library built-in, so the i18n generator and compiler need to be removed.

--- compilers/i18n/templates/i18n.h
+++
@@ -1,9 +0,0 @@
-#include <wchar.h>
-
-typedef struct Dict i18n_t;
-
-extern i18n_t *i18n_create(const wchar_t *locale);
-
-extern const wchar_t *i18n_get_text(i18n_t *i18n, const wchar_t *key);
-
-extern void i18n_destroy(i18n_t *i18n);
