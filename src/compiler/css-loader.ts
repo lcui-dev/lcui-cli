10f75c948b5fa94e9302e6058d3efd02c313a74f
Liu <lc-soft@live.cn>
2024-04-21 22:10:58 +0800

    feat: 从项目目录内加载 postcss 配置

--- src/compiler/css-loader.ts
+++ src/compiler/css-loader.ts
@@ -2,42 +2,54 @@ import path from "path";
 import postcss from "postcss";
 import postcssUrl from "postcss-url";
 import postcssModules from "postcss-modules";
+import { LoaderContext, LoaderInput, ModuleMetadata } from "../types.js";
+import { loadConfig } from "../utils.js";
 
-/** @type {Loader} */
-export default async function CSSLoader(content) {
+export default async function CSSLoader(
+  this: LoaderContext,
+  content: LoaderInput
+) {
   const loader = this;
   const { modules } = loader.getOptions();
   const cssText = `${content}`;
-  const result = await postcss(
-    [
-      postcssUrl({
-        async url(asset) {
-          try {
-            const outputPath = (
-              await loader.importModule(path.resolve(loader.context, asset.url))
-            ).default;
-            if (typeof outputPath === "string") {
-              return outputPath;
-            }
-          } catch (err) {
-            err.message = `url(${asset.url}):\n${err.message}`;
-            loader.emitError(err);
+  const processor = postcss().use(
+    postcssUrl({
+      async url(asset) {
+        try {
+          const outputPath = (
+            await loader.importModule(path.resolve(loader.context, asset.url))
+          ).default;
+          if (typeof outputPath === "string") {
+            return outputPath;
           }
-          loader.emitError(
-            new Error(
-              `url(${asset.url}): File does not exist or there is no matching loader`
-            )
-          );
-          return asset.url;
-        },
-      }),
-      modules &&
-        postcssModules({
-          exportGlobals: true,
-          getJSON() {},
-        }),
-    ].filter(Boolean)
-  )
+        } catch (err) {
+          err.message = `url(${asset.url}):\n${err.message}`;
+          loader.emitError(err);
+        }
+        loader.emitError(
+          new Error(
+            `url(${asset.url}): File does not exist or there is no matching loader`
+          )
+        );
+        return asset.url;
+      },
+    })
+  );
+  if (modules) {
+    processor.use(
+      postcssModules({
+        exportGlobals: true,
+        getJSON() {},
+      })
+    );
+  }
+  const customConfig = (await loadConfig(loader, "postcss")) as null | {
+    plugins?: postcss.AcceptedPlugin[];
+  };
+  if (customConfig && Array.isArray(customConfig.plugins)) {
+    customConfig.plugins.forEach((plugin) => processor.use(plugin));
+  }
+  const result = await processor
     .process(cssText, { from: loader.resourcePath })
     .async();
 
@@ -45,11 +57,10 @@ export default async function CSSLoader(content) {
     .parse(this.resourcePath)
     .name.replace(/[^a-zA-Z0-9]/g, "_")}`;
 
-  /** @type {ModuleMetadata} */
-  const metadata = {
+  const metadata: ModuleMetadata = {
     type: "asset",
     path: loader.resourcePath,
-    outputPath: loader.outputPath,
+    outputPath: loader.resourceOutputPath,
     headerFiles: ["<ui.h>", `"${loader.resourceOutputPath}"`],
     initCode: `ui_load_css_string(${ident}, ${JSON.stringify(
       path.relative(loader.context, loader.resourcePath)
@@ -79,7 +90,10 @@ export default async function CSSLoader(content) {
     .join("\\\n");
 
   return [
-    `/** Generated from: ${path.relative(path.dirname(this.resourceOutputPath), this.resourcePath)} */`,
+    `/** Generated from: ${path.relative(
+      path.dirname(this.resourceOutputPath),
+      this.resourcePath
+    )} */`,
     `static const char *${ident} = "\\`,
     `${cssStr.substring(1, cssStr.length - 1)}\\`,
     '";\n',
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,15 @@
-import path from 'path';
+import path from "path";
+import { cosmiconfig } from "cosmiconfig";
+import { LoaderContext } from "./types.js";
 
 export function toIdent(str: string) {
   return str.replace(/[^a-zA-Z0-9]/g, "_");
 }
 
-export function getResourceLoaderName(fileName: string, defaultComponentName?: string) {
+export function getResourceLoaderName(
+  fileName: string,
+  defaultComponentName?: string
+) {
   const ident = toIdent(defaultComponentName || fileName);
   return `ui_load_${ident}_resources`;
 }
@@ -15,4 +20,37 @@ export function parsePageRoute(context: string, filePath: string) {
     path: `/${dir.replaceAll(path.win32.sep, '/')}`,
     ident: toIdent(`${dir || "root"}_${name}`),
   };
+}
+
+export async function loadConfig(loaderContext: LoaderContext, moduleName: string) {
+  /** @see https://github.com/webpack-contrib/postcss-loader/blob/b1aecd9b18ede38b0ad4e693a94dadd2b2531429/src/utils.js#L51 */
+  const searchPlaces = [
+    // Prefer popular format
+    "package.json",
+    `${moduleName}.config.js`,
+    `${moduleName}.config.mjs`,
+    `${moduleName}.config.cjs`,
+    `.${moduleName}rc`,
+    `.${moduleName}rc.json`,
+    `.${moduleName}rc.js`,
+    `.${moduleName}rc.mjs`,
+    `.${moduleName}rc.cjs`,
+    `.${moduleName}rc.yaml`,
+    `.${moduleName}rc.yml`,
+    `.config/${moduleName}rc`,
+    `.config/${moduleName}rc.json`,
+    `.config/${moduleName}rc.yaml`,
+    `.config/${moduleName}rc.yml`,
+    `.config/${moduleName}rc.js`,
+    `.config/${moduleName}rc.cjs`,
+  ];
+  const explorer = await cosmiconfig(moduleName, {
+    searchStrategy: "global",
+    searchPlaces,
+  });
+  const result = await explorer.search(path.dirname(loaderContext.resourcePath));
+  if (!result || result.isEmpty) {
+    return null;
+  }
+  return result.config;
 }
